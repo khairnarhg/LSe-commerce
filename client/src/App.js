c66a10a0fae279c17f02595c019d4c68acdb420f
khairnarhg <hgkhairnar255@outlook.com>
2024-07-03 23:14:17 +0530

    coming soon hosted

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ComingSoon from './pages/ComingSoon/ComingSoon.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/comingSoon" element={<ComingSoon/>}/>
-
+          <Route path="/" element={<ComingSoon/>}/>
+          <Route path="/homePage" element={<HomePage/>}/>
         </Routes>
 
       </Router>
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+const homePage = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default homePage;
